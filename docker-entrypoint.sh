9c17b18c11f38f3e5f5afab4ce7908fe3f9c53e9
David Andersen <git@davidandersen.us>
2021-11-02 23:29:23 -0700

    simplify entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -Eeox pipefail
 id
-export BASE=/home/steam/stationeers
 export STEAMCMD=/usr/bin/steamcmd
+export APP_DIR=/opt/stationeers
+export BASE_DIR=/var/opt/stationeers
+export LOG_DIR=$STATE_DIR/logs
+export SAVE_DIR=$STATE_DIR/saves
 #STEAMCMD=/steamcmd/steamcmd.sh
 
 #if [ "$1" = 'rocketstation_DedicatedServer.x86_64' ] && [ "$(id -u)" = '0' ]; then
@@ -16,27 +19,27 @@ export STEAMCMD=/usr/bin/steamcmd
 if [ "$1" = 'rocketstation_DedicatedServer.x86_64' ]; then
 
   # update dedicated server
-  $STEAMCMD +login anonymous +force_install_dir $BASE +app_update 600760 -beta $BRANCH validate $STEAMCMD_EXTRA +quit
+  $STEAMCMD +login anonymous +force_install_dir $APP_DIR +app_update 600760 -beta $BRANCH validate $STEAMCMD_EXTRA +quit
 
   # create default.ini if it doesn't exist
-  if [ ! -f $BASE/default.ini ]; then
-    cp $BASE/rocketstation_DedicatedServer_Data/StreamingAssets/default.ini $BASE
+  #if [ ! -f $BASE/default.ini ]; then
+    #cp $BASE/rocketstation_DedicatedServer_Data/StreamingAssets/default.ini $BASE
     # change default password
-    sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$(pwgen 15 1)/g $BASE/default.ini
-  fi
+    #sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$(pwgen 15 1)/g $BASE/default.ini
+  #fi
 
-  rm -rf $BASE/core.*
-
+  rm -rf $APP_DIR/core.*
 fi
 
 if [ "$1" = 'rocketstation_DedicatedServer.x86_64' ]; then
-  export PATH=/home/steam/stationeers:$PATH
-  cd $BASE
-  if [ -f prestart.sh ]; then
-    chmod +x prestart.sh
-    ./prestart.sh
-  fi
-  exec "$@" -batchmode -nographics -autostart -basedirectory=/home/steam/stationeers | tee -a /home/steam/stationeers/stationeers.log
+  export PATH=$APP_DIR:$PATH
+  #cd $APP_DIR
+  #if [ -f prestart.sh ]; then
+  #  chmod +x prestart.sh
+  #  ./prestart.sh
+  #fi
+  exec "$@" -batchmode -nographics -autostart -basedirectory=$BASE_DIR
+  #| tee -a $LOG_DIR/stationeers.log
 else
   exec "$@"
 fi
